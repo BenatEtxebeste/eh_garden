8774a351db75f878694569059eff434430081fef
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-01-17 14:26:36 +0100

    menu empezado

--- src/principal/GestorArboles.java
+++ src/principal/GestorArboles.java
@@ -14,6 +14,49 @@ public class GestorArboles {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
 			
+			//declaracion de constantes (final) para las opciones
+			final int INSERTAR = 1;
+			final int ELIMINAR = 2;
+			final int MODIFICAR = 3;
+			final int VISUALIZAR = 4;
+			final int SALIR = 0;
+
+			Scanner teclado = new Scanner(System.in);
+			int opcion_menu;
+
+			do {
+				System.out.println("------MENU-------");
+				System.out.println(INSERTAR + ". Insertar arbol");
+				System.out.println(ELIMINAR + ". Eliminar arbol");
+				System.out.println(MODIFICAR + ". modificar informacion del arbol");
+				System.out.println(VISUALIZAR + ". visualizar arboles");
+				System.out.println(SALIR + ". Salir");
+				System.out.println("Elije una de las opciones");
+				opcion_menu = Integer.parseInt(scan.nextLine());
+
+				switch (opcion_menu) {
+				case INSERTAR:
+					System.out.println("primera opcion seleccionada\n");
+					break;
+				case ELIMINAR:
+					System.out.println("segunda opcion seleccionada\n");
+					break;
+				case MODIFICAR:
+					System.out.println("tercera opcion seleccionada\n");
+					break;
+				case VISUALIZAR:
+					System.out.println("cuarta opcion seleccionada\n");
+					break;
+				case SALIR:
+					System.out.println("ADIOS");
+					break;
+				default:
+					System.out.println("Opcion incorrecta!");
+				}
+
+			} while (opcion_menu != SALIR);
+			scan.close();
+			
 			Statement st = con.createStatement();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
