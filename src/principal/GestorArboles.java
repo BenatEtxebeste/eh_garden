4eee2a7b5a516c5bbb2635f7190367c889d0715d
Benat Etxebeste <Benat Etxebeste@HZ114834.H012108.NET>
2023-01-17 14:18:52 +0100

    connection done

--- src/principal/GestorArboles.java
+++ src/principal/GestorArboles.java
@@ -1,8 +1,23 @@
 package principal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class GestorArboles {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
